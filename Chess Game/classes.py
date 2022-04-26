7e56bf6df391f7c33518d6eebacc8041ef5b2207
jcolon030 <99977621+jcolon030@users.noreply.github.com>
2022-04-26 14:55:18 -0400

    Version 1.01

--- Chess Game/classes.py
+++ Chess Game/classes.py
@@ -1,15 +1,36 @@
 # Classes for each of the pieces
 
 
+from config import *
+
+
+
 class WhitePawn():
     def __init__(self, gameboard, cordPlane, position, end_position):
         self.gameboard = gameboard
         self.s = position
         self.d = end_position
         self.cordPlane = cordPlane
+        self.turn = []
+        self.first_move = True
 
     def double_space(self):
-        pass
+        if len(self.turn) == 0:
+            column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+            source = cordPlaneDict[self.s]
+            destination = (source[0]-2, source[1])
+
+
+
+            self.gameboard[source[0]][source[1]] = '  '
+            self.gameboard[destination[0]][destination[1]] = 'wp'
+            self.turn.append('moved')
+        
+            return self.gameboard
+        else:
+            print('This piece has already moved!')
+            return self.gameboard
 
     def single_space(self):
         column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
@@ -27,7 +48,7 @@ class WhitePawn():
         D2 = numEquivalentD
 
         self.gameboard[S1][S2] = '  '
-        self.gameboard[D1][D2] = source
+        self.gameboard[D1][D2] = source 
         
         return self.gameboard
 
@@ -56,12 +77,12 @@ class BlackPawn():
         numEquivalentS = column.index(self.s[0])
         numEquivalentD = column.index(self.d[0]) 
 
-        source = self.gameboard[8 - int(self.s[1])][numEquivalentS]
-        destination = self.gameboard[8 - int(self.d[1])][numEquivalentD]
+        source = self.gameboard[int(self.s[1])][numEquivalentS]
+        destination = self.gameboard[int(self.d[1])][numEquivalentD]
 
-        S1 = 8 - int(self.s[1])
+        S1 = int(self.s[1])
         S2 = numEquivalentS
-        D1 = 8 - int(self.d[1])
+        D1 = int(self.d[1])
         D2 = numEquivalentD
 
         self.gameboard[S1][S2] = ' '
@@ -77,3 +98,14 @@ class BlackPawn():
 
     def remove(self):
         pass
+
+
+
+if __name__ == "__main__":
+    my_pawn = WhitePawn(gameboard=gameboard,
+                        cordPlane=cordPlane,
+                        position="A2",
+                        end_position="-")
+    print(my_pawn.__dict__["gameboard"])
+    my_pawn.double_space()
+    print(my_pawn.__dict__["gameboard"])
--- Chess Game/main.py
+++ Chess Game/main.py
@@ -1,35 +1,6 @@
-from classes import WhitePawn
+from classes import WhitePawn, BlackPawn
 from in_game_settings import classes
-
-blackUnits = ['bR','bN','bB','bK','bQ','bB','bN','bR','bp']
-whiteUnits = ['wR','wN','wB','wK','wQ','wB','wN','wR','wp']
-
-gameboard = [['bR','bN','bB','bK','bQ','bB','bN','bR'],\
-             ['bp','bp','bp','bp','bp','bp','bp','bp'],\
-             ['  ','  ','  ','  ','  ','  ','  ','  '],\
-             ['  ','  ','  ','  ','  ','  ','  ','  '],\
-             ['  ','  ','  ','  ','  ','  ','  ','  '],\
-             ['  ','  ','  ','  ','  ','  ','  ','  '],\
-             ['wp','wp','wp','wp','wp','wp','wp','wp'],\
-             ['wR','wN','wB','wK','wQ','wB','wN','wR']]
-
-backupgameboard = [['bR','bN','bB','bK','bQ','bB','bN','bR'],\
-                   ['bp','bp','bp','bp','bp','bp','bp','bp'],\
-                   ['  ','  ','  ','  ','  ','  ','  ','  '],\
-                   ['  ','  ','  ','  ','  ','  ','  ','  '],\
-                   ['  ','  ','  ','  ','  ','  ','  ','  '],\
-                   ['  ','  ','  ','  ','  ','  ','  ','  '],\
-                   ['wp','wp','wp','wp','wp','wp','wp','wp'],\
-                   ['wR','wN','wB','wK','wQ','wB','wN','wR']]
-
-cordPlane = [['A8','B8','C8','D8','E8','F8','G8','H8'],\
-             ['A7','B7','C7','D7','E7','F7','G7','H7'],\
-             ['A6','B6','C6','D6','E6','F6','G6','H6'],\
-             ['A5','B5','C5','D5','E5','F5','G5','H5'],\
-             ['A4','B4','C4','D4','E4','F4','G4','H4'],\
-             ['A3','B3','C3','D3','E3','F3','G3','H3'],\
-             ['A2','B2','C2','D2','E2','F2','G2','H2'],\
-             ['A1','B1','C1','D1','E1','F1','G1','H1']]
+from config import *
 
 
 
@@ -58,23 +29,39 @@ def option_tree(selection):
     elif selection == '2':
         againstFriend = True
         whitesTurn = True
+        turn = 0
         while againstFriend != False:
             print_main_board(gameboard)
             coordinate = "ABCDEFGH"
             current_position = input("Input current position (ex. B5):\n")
             end_position = input('Where would you like to go (ex: C3):\n')
             piece = gameboard[8 - int(current_position[1])][coordinate.index(current_position[0])]
+            end_index = gameboard[8 - int(end_position[1])][coordinate.index(end_position[0])]
             if piece in whiteUnits:
-                player = classes[piece]
-                S1 = coordinate.index(current_position[0])
-                S2 = str(f'{coordinate[S1]}{str(int(current_position[1]) + 1)}')
-                if end_position == S2:
-                    player(gameboard, cordPlane, current_position, end_position).single_space()
+                if WhitePawn:
+                    player = classes[piece]
+                    S1 = coordinate.index(current_position[0])
+                    singlePushPosition = str(f'{coordinate[S1]}{str(int(current_position[1]) + 1)}')
+                    doublePushPosition = str(f'{coordinate[S1]}{str(int(current_position[1]) + 2)}')
+                    if end_index == gameboard[8 - int(current_position[1])][coordinate.index(end_position[0])]: # try statement for far end of board (out of index)
+                        if end_index == " ":
+                            WhitePawn.single_space()
+                    elif end_position == doublePushPosition:
+                        player(gameboard, cordPlane, current_position, end_position).double_space()
+                    else:
+                        print('Invalid Move')
                 else:
-                    print('Invalid Move')
+                    print("Please select again")
             elif piece in blackUnits:
-                player = classes[piece]
-                player(gameboard, cordPlane, current_position, end_position).single_space()
+                if BlackPawn:
+                    player = classes[piece]
+                    S1 = coordinate.index(current_position[0])
+                    S2 = str(f'{coordinate[S1]}{str(int(current_position[1]) + 1)}')
+                    if end_position == S2:
+                        player(gameboard, cordPlane, current_position, end_position).single_space()
+                        turn += 1
+                    else:
+                        print('Invalid Move')
             else:
                 print('Invalid Pick')
             #Done on new Patch
